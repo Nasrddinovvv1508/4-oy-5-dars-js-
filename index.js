f35ee80d857c925482f8030003c4044c6a77e9d1
unknown <najmiddinnasriddinov5@gmail.com>
2024-04-02 12:21:15 +0500

    All Done

---
+++ index.js
@@ -0,0 +1,72 @@
+    // 1-MASALA
+// let hour = +prompt(`Enter hour`);
+
+// function convertH(h) {
+//     return h * 60;
+// }
+
+// console.log(convertH(hour));
+
+
+    // 2-MASALA
+// let num = +prompt(`Enter a number`);
+
+// function additionInteger(arg) {
+//     return arg + 1;
+// }
+
+// console.log(additionInteger(num));
+
+
+    // 3-MASALA
+// let str = prompt(`Enter any text`);
+
+// function addSomething(str) {
+//     return `Something ${str}`
+// }
+
+// console.log(addSomething(str));
+
+
+    // 4-MASALA
+// let str = prompt(`Enter text`);
+// let arg = +prompt(`Enter argument`);
+
+// function cutStr() {
+//     return str.substring(arg);
+// }
+
+// console.log(cutStr());
+
+
+    // 5-MASALA
+// let a = +prompt('First number');
+// let b = +prompt('Second number');
+
+// function multiply() {
+//     return a * b;
+// }
+
+// alert(multiply());
+
+
+    // 6-MASALA
+// let year = +prompt(`Year`); // 2024
+// let month = +prompt(`Month`); // 4
+// let day = +prompt(`Day`); // 2
+// let calc = 0;
+
+// function hisob() {
+//     calc += ((year - 1970) * 365);
+
+//     if (day == 30) {
+//         calc += month * 30;
+//     }
+//     if (day < 30) {
+//         calc = calc + (30 * (month - 1) + (30 - day))
+//     }
+
+//     return calc;
+// }
+
+// console.log(hisob());
